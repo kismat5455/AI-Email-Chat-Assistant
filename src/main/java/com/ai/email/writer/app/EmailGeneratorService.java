fbc4bd6b1b02c0c690adbd0714b0e9dcd080ed13
Kismat Ali <alikismat2222@gmail.com>
2025-09-26 16:06:39 -0500

    Intital backend and frotnend complete

---
+++ src/main/java/com/ai/email/writer/app/EmailGeneratorService.java
@@ -0,0 +1,88 @@
+package com.ai.email.writer.app;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+
+import java.util.Map;
+
+@Service
+public class EmailGeneratorService {
+
+    private final WebClient webClient;
+
+    @Value("${gemini.api.url}")
+    private String geminiApiUrl;
+
+    @Value("${gemini.api.key}")
+    private String geminiApiKey;
+
+    public EmailGeneratorService(WebClient.Builder webClientBuilder) {
+        this.webClient = webClientBuilder.build();
+    }
+
+
+    public String generateEmailReply(EmailRequest emailRequest) {
+        // Build the promt
+        String promt = buildPromt(emailRequest);
+
+        // Craft a request
+        Map<String, Object> requestBody = Map.of(
+                "contents", new Object[]{
+                        Map.of("parts", new Object[]{
+                                Map.of("text", promt)
+                        })
+                }
+        );
+
+        // Do request and get response
+        String response = webClient.post()
+                .uri(geminiApiUrl)
+                .header("Content-Type","application/json")
+                .header("X-goog-api-key", geminiApiKey.trim())
+                .bodyValue(requestBody)
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+
+
+
+    // Extract response and return
+    return extreactResponseContent(response);
+    }
+
+    private String extreactResponseContent(String response) {
+        try {
+            ObjectMapper mapper= new ObjectMapper();
+            JsonNode rootNode= mapper.readTree(response);
+            return rootNode.path("candidates")
+                    .get(0)
+                    .path("content")
+                    .path("parts")
+                    .get(0)
+                    .path("text")
+                    .asText();
+        }catch( Exception e){
+            return "Error processong request: " +e.getMessage();
+        }
+    }
+
+
+
+
+    private String buildPromt(EmailRequest emailRequest) {
+
+
+        StringBuilder promt= new StringBuilder();
+        promt.append("Generate a professional email reply for the following email content. Please don't generate a subject line ");
+        if(emailRequest.getTone()!=null && !emailRequest.getTone().isEmpty()){
+            promt.append("USe a ").append(emailRequest.getTone()).append(" tone.");
+        }
+
+        promt.append("\n Original email: \n").append(emailRequest.getEmailContent());
+        return promt.toString();
+    }
+}
---
+++ src/main/java/com/ai/email/writer/app/EmailRequest.java
@@ -0,0 +1,13 @@
+package com.ai.email.writer.app;
+
+import lombok.Data;
+@Data
+public class EmailRequest {
+    private String emailContent;
+    private String tone;
+
+
+
+
+
+}
